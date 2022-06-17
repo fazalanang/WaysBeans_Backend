481d2007be03f080aca8c15fc4f3586828df95be
Your Name <faza.lanangdjati@gmail.com>
2022-06-18 02:09:39 +0700

    first commit

---
+++ src/routes/index.js
@@ -0,0 +1,64 @@
+const express = require("express");
+
+const router = express.Router();
+
+const {
+    register,
+    login,
+    checkAuth,
+    
+  } = require("../controllers/auth.js");
+  const {
+    addProduct,
+    getAllProducts,
+    getProduct,
+    updateProduct,
+    deleteProduct,
+  } = require("../controllers/product");
+
+  const {
+    addTransaction,
+    // addTransactionCart,
+    getTransactions,
+    deleteTransaction,
+    // notification,
+  } = require("../controllers/transaction");
+  const { getProfile, updateProfile } = require("../controllers/profile");
+  const {
+    getCart,
+    addCart,
+    deleteCart,
+    updateCart,
+  } = require("../controllers/cart");
+
+    const { auth } = require("../middlewares/auth");
+    const { uploadFile } = require("../middlewares/uploadFile");
+
+
+router.post("/register", register);
+router.post("/login", login);
+router.get("/check-auth", auth, checkAuth);
+// router.get("/users", getUsers);
+// router.delete("/user/:id", deleteUser);
+
+router.post("/product", auth, uploadFile("image"), addProduct);
+router.get("/products", getAllProducts);
+router.get("/product/:id", auth, getProduct);
+router.patch("/product/:id", auth, uploadFile("image"), updateProduct);
+router.delete("/product/:id", auth, deleteProduct);
+
+router.get("/profile", auth, getProfile);
+router.patch("/profile/:id", auth, uploadFile("image"), updateProfile);
+
+router.post("/cart", auth, addCart);
+router.get("/cart", auth, getCart);
+router.patch("/cart/:id", auth, updateCart);
+router.delete("/cart/:id", auth, deleteCart);
+
+router.post("/transaction", auth, addTransaction);
+// router.post("/transaction", auth, addTransactionCart);
+router.get("/transactions", auth, getTransactions);
+router.delete("/transaction/:id", auth, deleteTransaction);
+
+
+module.exports = router;
